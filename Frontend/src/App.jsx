caa33202d5684d4ac51e0bfe99d486ccd0f7d8ed
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2025-01-25 15:14:53 +0500

    routes added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import NavBar from './components/Navbar/NavBar';
 import './index.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
     </div>
   );
 }
