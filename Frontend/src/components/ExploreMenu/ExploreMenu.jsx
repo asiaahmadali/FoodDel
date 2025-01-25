322f643f000a8d30c26c335a909c9c9f9b840081
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2025-01-25 18:28:59 +0500

    explore menu added

--- Frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ Frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,5 +1,25 @@
+import { menu_list } from '../../assets/assets';
 function ExploreMenu() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Explore menu</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum,
+        accusantium?
+      </p>
+      {/* menu list */}
+      <div className="flex gap-2">
+        {menu_list.map((item, index) => {
+          return (
+            <div key={index}>
+              <img src={item.menu_image} alt="" />
+              <p>{item.menu_name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default ExploreMenu;
