d06d224f533e761335ceb357fbf37672b016ed71
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2025-01-25 12:47:09 +0500

    navbar added

--- Frontend/src/components/Navbar/NavBar.jsx
+++ Frontend/src/components/Navbar/NavBar.jsx
@@ -1,17 +1,50 @@
+import { useState } from 'react';
 import { assets } from '../../assets/assets';
+
 function NavBar() {
+  const [menu, setMenu] = useState('home');
   return (
     <div className="flex justify-between bg-gray-100 p-[20px]">
       <img src={assets.logo} alt="" />
-      <ul className="flex gap-4">
-        <li>Home</li>
-        <li>Menu</li>
-        <li>Mobile App</li>
-        <li>Contact Us</li>
+      <ul className="flex gap-4 font-outfit cursor-pointer">
+        <li
+          className={menu === 'home' ? 'active' : ''}
+          onClick={() => setMenu('home')}
+        >
+          Home
+        </li>
+        <li
+          className={menu === 'menu' ? 'active' : ''}
+          onClick={() => setMenu('menu')}
+        >
+          Menu
+        </li>
+        <li
+          className={menu === 'mobileapp' ? 'active' : ''}
+          onClick={() => setMenu('mobileapp')}
+        >
+          Mobile App
+        </li>
+        <li
+          className={menu === 'contactus' ? 'active' : ''}
+          onClick={() => setMenu('contactus')}
+        >
+          Contact Us
+        </li>
       </ul>
-      <div>
+      {/* right part of navbar */}
+      <div className="flex gap-2">
         <img src={assets.search_icon} alt="" />
-        <div></div>
+        <div className="relative ">
+          <img src={assets.bag_icon} alt="" />
+          <div className="absolute top-[-4px] right-[-4px] min-w-[10px] rounded-[5px] bg-red-600  min-h-[10px]"></div>
+        </div>
+        {/* sign in */}
+        <div>
+          <button className="bg-blue-500 p-2 transition-all duration-300 ease-in-out hover:bg-blue-700  text-white rounded-md">
+            Sign in
+          </button>
+        </div>
       </div>
     </div>
   );
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,11 +1,14 @@
 // tailwind.config.js
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}", // Ensure this matches your file extensions
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}', // Ensure this matches your file extensions
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      outfit: ['Outfit', 'sans-serif'],
+    },
   },
   plugins: [],
 };
