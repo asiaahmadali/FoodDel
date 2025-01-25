198dbefa2012b4ddf885d8a8e9cf0f59f074ea6f
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2025-01-25 21:06:37 +0500

    custom menu items

--- Frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ Frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,5 +1,5 @@
 import { menu_list } from '../../assets/assets';
-function ExploreMenu() {
+function ExploreMenu(Props) {
   return (
     <div className="mt-[30px] flex flex-col gap-[15px]">
       <h1 className="font-bold font-outfit text-2xl">Explore our menu</h1>
@@ -12,11 +12,23 @@ function ExploreMenu() {
       <div className="flex gap-10   mt-[10px] overflow-x-scroll hide-scrollbar ">
         {menu_list.map((item, index) => {
           return (
-            <div key={index} className="flex flex-col items-center gap-3">
+            <div
+              key={index}
+              onClick={() =>
+                Props.setCategory((prev) =>
+                  prev === item.menu_name ? 'All' : item.menu_name
+                )
+              }
+              className="flex flex-col items-center gap-3"
+            >
               <img
                 src={item.menu_image}
                 alt=""
-                className="min-w-[80px] cursor-pointer w-[7.5vw] rounded-[50%]"
+                className={
+                  Props.category === item.menu_name
+                    ? 'active min-w-[80px] cursor-pointer w-[7.5vw] rounded-[50%]'
+                    : 'min-w-[80px] cursor-pointer w-[7.5vw] rounded-[50%]'
+                }
               />
               <p className="font-outfit text-[#747474] text-[16px] font-[600]  cursor-pointer">
                 {item.menu_name}
--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Header from '../../components/Header/Header';
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
+import { useState } from 'react';
 function Home() {
+  const [category, setCategory] = useState('All');
   return (
     <div className="p-[100px]">
       <Header />
-      <ExploreMenu />
+      <ExploreMenu category={category} setCategory={setCategory} />
     </div>
   );
 }
