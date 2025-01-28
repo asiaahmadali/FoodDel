fe4b37660ed92a5aaa0e19efc9a6c826c9dd953c
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2025-01-29 04:49:39 +0500

    multer setup

--- Backend/routes/foodRoute.js
+++ Backend/routes/foodRoute.js
@@ -3,4 +3,8 @@ const foodRouter = express.Router();
 import addFood from '../controlers/foodController.js';
 import multer from 'multer';
 
+foodRouter.post('/add', (req, res) => {
+  addFood();
+});
+
 export default foodRouter;
