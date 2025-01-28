81a547f7977a70a184dcbedbb8bc6bdd33ce8a52
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2025-01-28 20:32:05 +0500

    server setup

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
 
+// middlewares
+app.use(express.json());
+app.use(cors());
+
 app.get('/', (req, res) => {
-  console.log('djkjkj');
+  res.send('api working');
 });
 
 app.listen(3000, () => {
