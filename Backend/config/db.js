da6779bc728ad774f3f63a8043bcda43889faf61
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2025-01-28 21:34:09 +0500

    db connected

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,5 +1,14 @@
 import mongoose from 'mongoose';
 
-const connectDB = async () => {
-  await mongoose.connect();
+export const connectDB = async () => {
+  await mongoose
+    .connect(
+      'mongodb+srv://asiaahmadali:asiaahmadali786@cluster0.aa0qs.mongodb.net/Zestify'
+    )
+    .then(() => {
+      console.log('db connected');
+    })
+    .catch(() => {
+      console.log('db not connected');
+    });
 };
--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import cors from 'cors';
+import { connectDB } from './config/db.js';
 const app = express();
 
 // middlewares
 app.use(express.json());
 app.use(cors());
+
+// db connection
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('api working');
