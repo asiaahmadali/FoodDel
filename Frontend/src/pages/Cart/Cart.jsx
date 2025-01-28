6f305261c6c3477985d8b07ae4313e2b4e308950
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2025-01-28 15:21:12 +0500

    items added successfully

--- Frontend/src/pages/Cart/Cart.jsx
+++ Frontend/src/pages/Cart/Cart.jsx
@@ -4,30 +4,48 @@ import { StoreContext } from '../../context/StoreContext';
 function Cart() {
   const { food_list, cartitems, removeFromCart } = useContext(StoreContext);
   return (
-    <div>
+    <div className="m-[120px] font-outfit">
       {/* cart items */}
-      <div>
+      <div className="flex flex-col">
         {/* items title */}
-        <div>
-          <p>Items</p>
+        <div className="grid grid-cols-6 gap-4 mb-[10px] items-center">
+          <p>Item</p>
           <p>Title</p>
           <p>Quantity</p>
           <p>Price</p>
           <p>Total</p>
           <p>Remove</p>
         </div>
-        <hr />
+        <hr className="mb-[10px] h-[2px]" />
         {food_list.map((item, index) => {
           if (cartitems[item._id] > 0) {
             return (
-              <div key={index}>
-                <img src={item.image} alt="" />
-                <p>{item.name}</p>
-                <p>{item.price}</p>
-                <p>{cartitems[item._id]}</p>
-                <p>{item.price * cartitems[item._id]}</p>
-                <p>x</p>
-              </div>
+              <>
+                <div
+                  key={index}
+                  className="grid grid-cols-6 gap-4 items-center mb-[20px]"
+                >
+                  <div className="flex justify-start">
+                    <img src={item.image} alt="" className="w-[80px]" />
+                  </div>
+
+                  <p>{item.name}</p>
+
+                  <p>{cartitems[item._id]}</p>
+
+                  <p>{'$' + item.price}</p>
+
+                  <p>{'$' + item.price * cartitems[item._id]}</p>
+
+                  <p
+                    className="cursor-pointer font-bold"
+                    onClick={() => removeFromCart(item._id)}
+                  >
+                    x
+                  </p>
+                </div>
+                <hr className="my-[5px] " />
+              </>
             );
           }
         })}
