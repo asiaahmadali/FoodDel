fb61daec32fa867bb0c90592d08838ff6e834c51
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2025-01-29 04:22:21 +0500

    foodrouter

---
+++ Backend/controlers/foodController.js
@@ -0,0 +1,10 @@
+import foodModel from '../models/foodmodel.js';
+import fs from 'fs';
+
+// add food
+
+const addFood = async (req, res) => {
+  await foodModel;
+};
+
+module.exports = addFood;
---
+++ Backend/routes/foodRoute.js
@@ -0,0 +1,8 @@
+import express from 'express';
+const foodRouter = express.Router();
+import addFood from '../controlers/foodController.js';
+import multer from 'multer';
+
+foodRouter.use();
+
+module.exports = foodRouter;
