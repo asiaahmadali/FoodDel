2fda42af973c440877de38045a43ca3c739e62b1
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2025-01-29 07:27:31 +0500

    multer completed

--- Backend/routes/foodRoute.js
+++ Backend/routes/foodRoute.js
@@ -3,7 +3,18 @@ const foodRouter = express.Router();
 import addFood from '../controlers/foodController.js';
 import multer from 'multer';
 
-foodRouter.post('/add', (req, res) => {
+// multer
+
+const storage = multer.diskStorage({
+  destination: 'uploads',
+  filename: (rreq, file, cb) => {
+    return cb(null, `${Date.now()}${file.originalname}`);
+  },
+});
+
+const uploads = multer({ storage: storage });
+
+foodRouter.post('/add', uploads.single('image'), (req, res) => {
   addFood();
 });
 
