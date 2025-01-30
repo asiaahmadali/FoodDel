930ce8dc1745a2ebf519096109be86b5c21dd5d9
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2025-01-30 11:33:45 +0500

    addproduct UI completed

--- Admin/src/pages/AddProduct/AddProduct.jsx
+++ Admin/src/pages/AddProduct/AddProduct.jsx
@@ -1,57 +1,117 @@
+import { useEffect, useState } from 'react';
 import { assets } from '../../assests/assets';
 
 function AddProduct() {
+  const [image, setImage] = useState(false);
+  const [data, setData] = useState({
+    name: '',
+    description: '',
+    price: '',
+    category: 'Salad',
+  });
+
+  //   changeHandler
+
+  const changeHandler = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setData((data) => ({ ...data, [name]: value }));
+  };
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
   return (
     <div>
-      <form>
+      <form className="m-[30px]  text-[#6d6d6d] flex flex-col gap-[10px] w-[70%] font-outfit text-[15px]">
         {/* product image */}
-        <div className="flex flex-col">
+        <div className="flex flex-col w-[100px] gap-[10px] ">
           <h1>Upload image</h1>
-          <label htmlFor="image">
-            <img src={assets.upload_area} alt="" />
+          <label htmlFor="image" className="cursor-pointer">
+            <img
+              src={image ? URL.createObjectURL(image) : assets.upload_area}
+              alt=""
+              className="w-[100px] h-[90px]"
+            />
           </label>
-          <input type="file" id="image" required hidden />
+          <input
+            type="file"
+            id="image"
+            required
+            hidden
+            onChange={(e) => setImage(e.target.files[0])}
+          />
         </div>
 
         {/* product name */}
-        <div>
+        <div className="flex flex-col w-[30vw] gap-[8px]">
           <p>product name</p>
-          <input type="text" name="name" placeholder="product name" required />
+          <input
+            type="text"
+            name="name"
+            placeholder="product name"
+            required
+            onChange={changeHandler}
+            value={data.name}
+            className="outline-none border-[1px] border-black border-opacity-[60%] rounded-md p-[10px]"
+          />
         </div>
 
         {/* product description */}
-        <div>
+        <div className="flex flex-col w-[30vw] gap-[8px]">
           <p>Product description</p>
           <textarea
             name="description"
             rows={5}
+            onChange={changeHandler}
+            value={data.description}
             placeholder="write decription...."
             aria-required
+            className="outline-none border-[1px] border-black border-opacity-[60%] rounded-md p-[10px]"
           ></textarea>
         </div>
 
-        {/* product cagtegory */}
-        <div>
-          <p>product category</p>
-          <select name="category">
-            <options value="Salad">Salad</options>
-            <options value="Rolls">Rolls</options>
-            <options value="Deserts">Deserts</options>
-            <options value="Sandwich">Sandwich</options>
-            <options value="Cake">Cake</options>
-            <options value="Pure Veg">Pure Veg</options>
-            <options value="Pasta">Pasta</options>
-            <options value="Noodles">Noodles</options>
-          </select>
+        <div className="flex gap-[20px] w-[30vw] ">
+          {/* product cagtegory */}
+          <div className="flex flex-col gap-[8px]">
+            <p>product category</p>
+            <select
+              name="category"
+              onChange={changeHandler}
+              value={data.category}
+              className="outline-none border-[1px] border-black border-opacity-[60%] rounded-md p-[10px]"
+            >
+              <option value="Salad">Salad</option>
+              <option value="Rolls">Rolls</option>
+              <option value="Deserts">Deserts</option>
+              <option value="Sandwich">Sandwich</option>
+              <option value="Cake">Cake</option>
+              <option value="Pure Veg">Pure Veg</option>
+              <option value="Pasta">Pasta</option>
+              <option value="Noodles">Noodles</option>
+            </select>
+          </div>
+
+          {/* product price */}
+          <div className="flex flex-col gap-[8px]">
+            <p>Product Price</p>
+            <input
+              type="Number"
+              name="price"
+              onChange={changeHandler}
+              value={data.price}
+              placeholder="$10"
+              className="outline-none border-[1px] border-black border-opacity-[60%] rounded-md p-[10px]"
+            />
+          </div>
         </div>
 
-        {/* product price */}
-        <div>
-          <p>Product Price</p>
-          <input type="Number" name="price" placeholder="$10" />
-        </div>
-
-        <button type="submit">Add</button>
+        <button
+          type="submit"
+          className="bg-orange-500 p-[10px] w-[30vw] mt-[30px] rounded-md text-white"
+        >
+          Add
+        </button>
       </form>
     </div>
   );
