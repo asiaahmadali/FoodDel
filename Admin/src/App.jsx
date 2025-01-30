d376f18cb3bb3f0fc69528800f60d0982af4cb3e
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2025-01-30 09:26:08 +0500

    assests added

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -2,7 +2,7 @@ import './index.css';
 function App() {
   return (
     <div>
-      <h1 className="text-blue-800 text-2xl">admin</h1>
+      <h1 className="text-blue-800 text-2xl font-outfit">admin</h1>
     </div>
   );
 }
