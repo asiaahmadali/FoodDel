83391f819b3a2d6c179f337b5e71f04786faaccd
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2025-01-30 14:42:00 +0500

    jwt

--- Backend/controlers/userController.js
+++ Backend/controlers/userController.js
@@ -7,6 +7,29 @@ import validator from 'validator';
 const Login = async () => {};
 
 // Signup
-const SignUp = () => {};
+const SignUp = async () => {
+  const { name, email, password } = req.body;
+  try {
+    // if user already with same email
+    const isuseralready = await userModel.findOne({ email });
+    if (isuseralready) {
+      return res.json({ success: false, message: 'user already exist' });
+    }
+
+    // validating  email foramtting
+    if (!validator.isEmail(email)) {
+      return res.json({ success: false, message: 'please enter valid email' });
+    }
+
+    // validating  password strength
+    if (password.length < 8) {
+      return res.json({ success: false, message: 'enter strong password' });
+    }
+
+    // password encryption
+    const salt = await bcrypt.salt(12);
+    const encPassword = await bcrypt.hash(password, salt);
+  } catch (error) {}
+};
 
 export { Login, SignUp };
