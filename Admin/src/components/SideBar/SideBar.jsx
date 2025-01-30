6f771a44898d02ca11604d6cba2468fecf115a42
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2025-01-30 10:04:26 +0500

    pages added

--- Admin/src/components/SideBar/SideBar.jsx
+++ Admin/src/components/SideBar/SideBar.jsx
@@ -2,20 +2,20 @@ import { assets } from '../../assests/assets';
 
 function SideBar() {
   return (
-    <div className="flex flex-col gap-[15px] font-outfit p-[90px] border-[1px] border-[#a9a9a9] border-top-0 min-h-[100vh] w-[20%]">
-      <div>
-        <img src={assets.add_icon} alt="" />
-        <p>Add Items</p>
+    <div className="flex flex-col gap-[15px] font-outfit p-[60px] border-[1px] border-[#a9a9a9] border-top-0 min-h-[100vh] w-[20%]">
+      <div className="flex items-center gap-[13px] border-[1px] border-[#a9a9a9] cursor-pointer rounded-sm w-full p-[10px]">
+        <img src={assets.add_icon} alt="" className="w-[25px] h-[25px]" />
+        <p className="hidden md:block">Add Items</p>
       </div>
 
-      <div>
-        <img src={assets.order_icon} alt="" />
-        <p>List Items</p>
+      <div className="flex items-center gap-[13px] border-[1px] border-[#a9a9a9] cursor-pointer rounded-sm w-full p-[10px]">
+        <img src={assets.order_icon} alt="" className="w-[25px] h-[25px]" />
+        <p className="hidden md:block">List Items</p>
       </div>
 
-      <div>
-        <img src={assets.order_icon} alt="" />
-        <p>Orders</p>
+      <div className="flex items-center gap-[13px] border-[1px] border-[#a9a9a9] cursor-pointer rounded-sm w-full p-[10px]">
+        <img src={assets.order_icon} alt="" className="w-[25px] h-[25px]" />
+        <p className="hidden md:block">Orders</p>
       </div>
     </div>
   );
---
+++ Admin/src/pages/ListProducts/ListProducts.jsx
@@ -0,0 +1,5 @@
+function ListProducts() {
+  return <div></div>;
+}
+
+export default ListProducts;
