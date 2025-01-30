7fb118c91710d046786a6e0dafb8c69797804662
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2025-01-30 09:12:05 +0500

    admin page started

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,7 +1,8 @@
+import './index.css';
 function App() {
   return (
     <div>
-      <h1>admin page</h1>
+      <h1 className="text-blue-800 text-2xl">admin</h1>
     </div>
   );
 }
---
+++ Admin/tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}', // Ensure this matches your file extensions
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      outfit: ['Outfit', 'sans-serif'],
+    },
+  },
+
+  plugins: [],
+};
