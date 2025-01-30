afdb316ff209cd2b00b3cb261f0be2bfc859e9db
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2025-01-30 14:19:11 +0500

    user router added

---
+++ Backend/controlers/userController.js
@@ -0,0 +1,12 @@
+import userModel from '../models/usermodel.js';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import validator from 'validator';
+
+// login user
+const Login = async () => {};
+
+// Signup
+const SignUp = () => {};
+
+export { Login, SignUp };
