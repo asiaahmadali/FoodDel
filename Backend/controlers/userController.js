58b30309075ab416424bd7db5bb17385a131865e
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2025-01-30 15:01:23 +0500

    signup completed

--- Backend/controlers/userController.js
+++ Backend/controlers/userController.js
@@ -4,10 +4,14 @@ import bcrypt from 'bcrypt';
 import validator from 'validator';
 
 // login user
-const Login = async () => {};
+const Login = async (req, res) => {};
+
+const TokenGenerate = (id) => {
+  jwt.sign({ id }, process.env.SECRET_KEY);
+};
 
 // Signup
-const SignUp = async () => {
+const SignUp = async (req, res) => {
   const { name, email, password } = req.body;
   try {
     // if user already with same email
@@ -27,9 +31,27 @@ const SignUp = async () => {
     }
 
     // password encryption
-    const salt = await bcrypt.salt(12);
+    const salt = await bcrypt.genSalt(12);
     const encPassword = await bcrypt.hash(password, salt);
-  } catch (error) {}
+
+    // create user
+    const createdUser = await userModel.create({
+      name,
+      email,
+      password: encPassword,
+    });
+
+    // save user in database
+    const user = await createdUser.save();
+    // token
+    const token = TokenGenerate(user._id);
+    // send this token as response
+    res.json({ success: true, token });
+    //
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: 'error' });
+  }
 };
 
 export { Login, SignUp };
--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import { connectDB } from './config/db.js';
-const app = express();
 import foodRouter from './routes/foodRoute.js';
 import userRouter from './routes/userRoute.js';
+import 'dotenv/config';
+const app = express();
+
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
