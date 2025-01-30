cf5713301c39b3794ad7908d0a9a2bc1f9cc4779
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2025-01-30 13:38:28 +0500

    items listing complete

--- Admin/src/pages/ListProducts/ListProducts.jsx
+++ Admin/src/pages/ListProducts/ListProducts.jsx
@@ -17,51 +17,65 @@ function ListProducts() {
     }
   };
 
+  const removeItem = async (itemid) => {
+    const response = await axios.post(`${url}/api/food/delete`, { id: itemid });
+    await fetchList();
+    if (response.data.success) {
+      toast.success(response.data.message);
+    } else {
+      toast.error('error');
+    }
+  };
+
   useEffect(() => {
     fetchList();
   }, []);
 
   return (
-    <div>
-      <div className="flex flex-col p-[20px] font-outfit">
-        <h1 className="text-2xl font-bold mb-[20px]">All Food List</h1>
-        {/* items title */}
-        <div className="grid grid-cols-6 gap-4 mb-[10px] items-center">
-          <p>Image</p>
-          <p>Name</p>
-          <p>Category</p>
-          <p>Price</p>
-          <p>Action</p>
-        </div>
-        <hr className="mb-[10px] h-[2px]" />
+    <div className="flex flex-col m-[40px] font-outfit w-[70vw] ">
+      <h1 className="text-2xl font-bold mb-[20px]">All Food List</h1>
+      {/* items title */}
 
-        {/* Map over the list */}
-        {list.length > 0 ? (
-          list.map((item, index) => {
-            return (
-              <div
-                key={index}
-                className="grid grid-cols-6 gap-4 items-center mb-[20px]"
-              >
-                <div className="flex justify-start">
-                  <img
-                    src={`${url}/images/${item.image}`}
-                    alt={item.name}
-                    className="w-[80px]"
-                  />
-                </div>
-
-                <p>{item.name}</p>
-                <p>{item.category}</p>
-                <p>{'$' + item.price}</p>
-                <p>x</p>
-              </div>
-            );
-          })
-        ) : (
-          <p>No food items found.</p>
-        )}
+      <div className="grid grid-cols-6 gap-4 border-[1px] border-black border-b-0 border-opacity-[70%] p-[10px] bg-gray-100 items-center">
+        <p>Image</p>
+        <p>Name</p>
+        <p>Category</p>
+        <p>Price</p>
+        <p>Action</p>
       </div>
+      <hr className=" h-[2px]" />
+
+      {/* Map over the list */}
+      {list.length > 0 ? (
+        list.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className="grid grid-cols-6 items-center p-[10px] border-[1px] border-black border-opacity-[70%]"
+            >
+              <div className="flex justify-start">
+                <img
+                  src={`${url}/images/${item.image}`}
+                  alt={item.name}
+                  className="w-[60px] h-[50px]"
+                />
+              </div>
+
+              <p>{item.name}</p>
+              <p>{item.category}</p>
+              <p>{'$' + item.price}</p>
+              <p
+                className="cursor-pointer"
+                onClick={() => removeItem(item._id)}
+              >
+                x
+              </p>
+            </div>
+          );
+        })
+      ) : (
+        <p>No food items found.</p>
+      )}
     </div>
   );
 }
