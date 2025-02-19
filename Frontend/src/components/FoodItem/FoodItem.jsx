9227e3fbe293f5e522cc48d0ad9b77603ee3238e
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2025-02-19 22:15:22 +0500

    added

--- Frontend/src/components/FoodItem/FoodItem.jsx
+++ Frontend/src/components/FoodItem/FoodItem.jsx
@@ -9,9 +9,7 @@ function FoodItem(Props) {
       {/* food item image */}
       <div className="relative">
         <img
-          src={
-            `https://quick-bite-navy.vercel.app/uploads/` + Props.fooditem.image
-          }
+          src={`http://localhost:3000/images/` + Props.fooditem.image}
           alt="an image"
           className="rounded-t-md w-full h-[220px] object-cover"
         />
