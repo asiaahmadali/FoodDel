6129a8859a4dfcda1fd22eb7ec0c14f33bb7d283
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2025-02-19 22:12:13 +0500

    added

--- Backend/index.js
+++ Backend/index.js
@@ -10,6 +10,8 @@ import 'dotenv/config';
 import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
+// static files
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
