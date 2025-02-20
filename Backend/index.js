36418d220ea44efcee8470426d649e8905f5205d
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2025-02-20 16:32:54 +0500

    token headers

--- Backend/index.js
+++ Backend/index.js
@@ -19,12 +19,22 @@ const __dirname = dirname(__filename);
 
 // ✅ Step 1: Apply CORS Middleware (Always Set Headers)
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://quick-bite-frontendside.vercel.app');
+  res.header(
+    'Access-Control-Allow-Origin',
+    'https://quick-bite-frontendside.vercel.app'
+  );
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
+
   next();
 });
+// token headers
+app.use(
+  cors({
+    allowedHeaders: ['Content-Type', 'Authorization', 'token'],
+  })
+);
 
 // ✅ Middleware
 app.use(express.json());
